19560a0e82b4065e8b99d385f29432b8f5b58c90
chenglu <chenglu.she@gmail.com>
2024-04-17 19:05:42 +0800

    add op port

--- apphub/tensorboard.py
+++ apphub/tensorboard.py
@@ -1,7 +1,6 @@
 import os
 
 import gradio as gr
-
 from apphub.app import App, AppOption
 
 
@@ -22,6 +21,11 @@ class Tensorboard(App):
         """应用的端口号"""
         return 20005
 
+    @property
+    def op_port(self):
+        """应用的端口号"""
+        return 30005
+
     def render_installation_page(self) -> "gr.Blocks":
         with gr.Blocks() as b:
             gr.Markdown(
@@ -31,10 +35,10 @@ TensorBoard 可视化面板
 """
             )
             install_location = self.render_install_location(allow_work=True)
-            conda_env = self.render_conda_env_selector(info="尽量就直接用 base 环境安装 TensorBoard，除非你知道自己在做什么。")
-            self.render_installation_button(
-                inputs=[install_location, conda_env]
+            conda_env = self.render_conda_env_selector(
+                info="尽量就直接用 base 环境安装 TensorBoard，除非你知道自己在做什么。"
             )
+            self.render_installation_button(inputs=[install_location, conda_env])
             self.render_log()
         return b
 
@@ -48,14 +52,25 @@ TensorBoard 可视化面板
     def render_start_page(self) -> "gr.Blocks":
         with gr.Blocks() as b:
             gr.Markdown("""# 启动 TensorBoard""")
-            log_path = gr.Textbox("", label="log events 目录的绝对路径", info="TensorBoard 的 Log Events 的绝对路径，注意是指定的是目录不是文件，另外最好是绝对路径。如果是相对路径，会以家目录做为相对位置。")
-            other_boot_command = gr.Textbox("", label="其他启动项", info="一般留空即可。注意：不要自己指定 --port 或 --ip。")
+            log_path = gr.Textbox(
+                "",
+                label="log events 目录的绝对路径",
+                info="TensorBoard 的 Log Events 的绝对路径，注意是指定的是目录不是文件，另外最好是绝对路径。如果是相对路径，会以家目录做为相对位置。",
+            )
+            other_boot_command = gr.Textbox(
+                "",
+                label="其他启动项",
+                info="一般留空即可。注意：不要自己指定 --port 或 --ip。",
+            )
             self.render_start_button([log_path, other_boot_command])
         return b
 
     def start(self, log_path, other_boot_command):
         with self.conda_activate(self.cfg.conda_env):
-            self.execute_command(f"tensorboard --logdir={log_path} --bind_all --port {self.port} {other_boot_command}", daemon=True)
+            self.execute_command(
+                f"tensorboard --logdir={log_path} --bind_all --port {self.port} {other_boot_command}",
+                daemon=True,
+            )
         self.app_started()
 
 
